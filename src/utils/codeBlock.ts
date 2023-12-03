2e4da25ea780fc046c6eba7c8c8c6288ad6bdcf7
KuroXI <vgonzales5157@gmail.com>
2023-12-03 10:41:27 +0800

    [dev] feat: voice log join/left/change

--- src/utils/codeBlock.ts
+++ src/utils/codeBlock.ts
@@ -1,3 +1,3 @@
-export const CodeBlock = (text: string, type: string = "yaml") => {
+export const CodeBlock = (text: string = "", type: string = "yaml") => {
 	return `\`\`\`${type}\n${text}\`\`\``;
 };
